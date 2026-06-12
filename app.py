c82410f90674ea4f62cbaff0a565929ca4a85ee3
bamorimfragozo-png <bamorimfragozo@gmail.com>
2026-06-12 16:11:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -276,13 +276,20 @@ if (not st.session_state.dadosCarregados):
             with st.spinner("Processando arquivos e atualizando planilhas de notas..."):
                 BDNovo=extrairDados(arquivosEnviados)
 
+linkSalaAtiva = DICIONARIO_SALAS[salaSelecionada]
+
+
+
+
+
+                linkSalaAtiva=DICIONARIO_SALAS[salaSelecionada]
                 #codigo adicionado para teste
                 df_atual = conn.read(spreadsheet=linkSalaAtiva)
                 df_final = pd.concat([df_atual, BDNovo],ignore_index=True)
                 #conn.update(spreadsheet=linkSalaAtiva, data=df_final )
 
                 if (not BDNovo.empty):
-                    linkSalaAtiva=DICIONARIO_SALAS[salaSelecionada]
+                    #linkSalaAtiva=DICIONARIO_SALAS[salaSelecionada]
                     #conn.update(spreadsheet=linkSalaAtiva, data=BDNovo) 
                     conn.update(spreadsheet=linkSalaAtiva, data=df_final) #<----teste
                     st.session_state.salaAtiva=salaSelecionada
