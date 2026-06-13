b08c0420a23d12bb5e4852da49517624454aec70
bamorimfragozo-png <bamorimfragozo@gmail.com>
2026-06-13 15:11:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if ("materiaSelecionada" not in st.session_state):
 if ("resetObs" not in st.session_state):
     st.session_state.resetObs=0
 if ("salaAtiva" not in st.session_state):
-    st.session_state.salaSelecionada="Redes 1"
+    st.session_state.salaAtiva="Redes 1"
 if ("fotoAluno" not in st.session_state):
     st.session_state.fotoAluno={}
 if ("ordenacao" not in st.session_state):
@@ -265,6 +265,11 @@ def extrairDados(arquivosPdf):
 
 #UPLOAD DOS RELATÓRIOS EM PDF
 if (not st.session_state.dadosCarregados):
+    if (st.sidebar.button("Ir para Dashboard")):
+        st.session_state.dadosCarregados=True
+        st.session_state.materiaSelecionada=None
+        st.session_state.numAluno=0
+        st.rerun()
     st.title("Upload de PDFs")
     st.subheader("Selecione a sala correspondente e faça o upload dos relatórios em PDF.")
 
@@ -299,7 +304,7 @@ if (not st.session_state.dadosCarregados):
 
 #EXIBIÇÃO VISUAL DO DASHBOARD ACADÊMICO
 else:
-    if (st.sidebar.button("Voltar para Tela de Upload")):
+    if (st.sidebar.button("Ir para Tela de Upload")):
         st.session_state.dadosCarregados=False
         st.session_state.numAluno=0
         st.session_state.materiaSelecionada=None
