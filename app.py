95ee2e3e94e6eef59205d6e26af9d8227fb09989
bamorimfragozo-png <bamorimfragozo@gmail.com>
2026-06-14 12:56:23 -0300

    Update sidebar display for student list
    Comment out the number of students display and show the list of students instead.

--- app.py
+++ app.py
@@ -338,8 +338,8 @@ else:
     ordemChamada=BD.sort_values(by='Nº Chamada', ascending=True)
     alunosLista=ordemChamada['Aluno'].unique().tolist()
     
-    st.sidebar.write(f"numeroAlunos:{len(alunosLista)}")
-    #st.sidebar.write(alunosLista)
+    #st.sidebar.write(f"numeroAlunos:{len(alunosLista)}")
+    st.sidebar.write(alunosLista)
     
     if (st.session_state.numAluno>=len(alunosLista)):
         st.session_state.numAluno=0
