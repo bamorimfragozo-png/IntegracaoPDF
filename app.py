3a68b3d16504cc99e5d38e366bce6de2d47e1ba8
bamorimfragozo-png <bamorimfragozo@gmail.com>
2026-06-26 13:51:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -168,9 +168,7 @@ def extrairDados(arquivosPdf):
             partes = linha.split(":")
             if (len(partes) > 1):
                 serieAluno = partes[1].strip()[:27]
-                nomeAluno = ""
 
-                serieAluno = ""
         return extrair_linhas_metadados(idx + 1, linhas, nomeAluno, serieAluno)
 
     def verificar_filtro_palavras(linha):
